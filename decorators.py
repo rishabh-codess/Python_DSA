70c4e556f46be8027d389a83ee23de8dd4c96f65
rishabh-codess <info.rishabhkumar16@gmail.com>
2026-08-09 00:16:38 +0530

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,14 @@
+#decorators with arguments
+def repeat (n):
+    def decorator(func):
+        def wrapper(*args,**kwargs):
+            for _ in range (n):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@repeat (3)
+def say_hello():
+    print("hello")
+
+say_hello()
