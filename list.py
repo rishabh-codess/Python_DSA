84e0bcae829ff81440d18a7b158cbf216650c6db
rishabh-codess <info.rishabhkumar16@gmail.com>
2026-08-09 00:20:54 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+to_do_list= ["study python","post github", "update linkdin", "watch movies"]
+to_do_list.append("go to gym")
+to_do_list.append("buy fruits")
+to_do_list.remove("watch movies")
+if "post github" in to_do_list:
+    print ("finish the remaining work")
+print(f"/{to_do_list}")
