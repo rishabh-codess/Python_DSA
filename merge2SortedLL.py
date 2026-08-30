e0ea12fd5ce5d06cbd356fe2a483444656d90cbb
rishabh-codess <info.rishabhkumar16@gmail.com>
2026-08-30 23:21:20 +0530

    Add files via upload

--- merge2SortedLL.py
+++ merge2SortedLL.py
@@ -1,39 +1,39 @@
 from commonLL import *
 
-def Merger2sortedll(head1,head2):
-    
+def Merger2sortedll(head1, head2):
     if head1 is None:
         return head2
     if head2 is None:
         return head1
-    
-    finalhead=None
-    finaltail=None
-    
+
+    finalhead = None
+    finaltail = None
+
     while head1 is not None and head2 is not None:
-        if(head1.data<head2.data):
-            if(finalhead==None):
-                finalhead=head1
-                finaltail=head1
+        if head1.data < head2.data:
+            if finalhead == None:
+                finalhead = head1
+                finaltail = head1
             else:
-                finaltail.next=head1
-                finaltail=head1
+                finaltail.next = head1
+                finaltail = head1
+            head1 = head1.next  # <-- FIX 1: Advance head1
         else:
-            if (finalhead==None):
-                finalhead=head2
-                finaltail=head2
+            if finalhead == None:
+                finalhead = head2
+                finaltail = head2
             else:
                 finaltail.next = head2
-                finalhead=head2
-            head2=head2.next
+                finaltail = head2
+            head2 = head2.next  # Already present in your code
 
-	if head1 is not None:
-            finaltail.next=head1
+    # <-- FIX 2: These must be outside the while loop
+    if head1 is not None:
+        finaltail.next = head1
 
-	if head2 is not None:
-            finaltail.next=head2
+    if head2 is not None:
+        finaltail.next = head2
 
-		return finalhead
-        
+    return finalhead
             
             
